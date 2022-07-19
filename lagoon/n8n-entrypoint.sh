729df5c32aa027644e4f012e8f506b4e054253df
Bryan Gruneberg <hello@bryangruneberg.com>
2022-07-19 23:51:55 +0000

    Fixes to setup

--- lagoon/n8n-entrypoint.sh
+++ lagoon/n8n-entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-echo "Running N8N specific entrypoint"
-
-if [ -d /root/.n8n ] ; then
-  chmod o+rx /root
-  chown -R node /root/.n8n
-  ln -s /root/.n8n /home/node/
-fi
-
-echo "Settng N8N_EDITOR_BASE_URL to $LAGOON_ROUTE"
-export N8N_EDITOR_BASE_URL=$LAGOON_ROUTE
-
-chown -R node /home/node
+echo "Running N8N specific entrypoint..."
+. /app/n8n-lagoon-vars
